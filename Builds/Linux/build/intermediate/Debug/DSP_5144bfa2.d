32016a2ae13bd9f8ac2633f77e3e5f6a0f9f1f50
tomchaossen <thomas.kaussen@firemail.de>
2015-05-07 09:07:55 +0200

    new

---
+++ Builds/Linux/build/intermediate/Debug/DSP_5144bfa2.d
@@ -0,0 +1,2 @@
+build/intermediate/Debug/DSP_5144bfa2.o: ../../Source/DSP.cpp \
+ ../../Source/DSP.h
---
+++ Builds/Linux/build/intermediate/Debug/juce_VST3_Wrapper_7b342dad.d
@@ -0,0 +1,3 @@
+build/intermediate/Debug/juce_VST3_Wrapper_7b342dad.o: \
+ ../../../../../../juce/modules/juce_audio_plugin_client/VST3/juce_VST3_Wrapper.cpp \
+ ../../JuceLibraryCode/AppConfig.h
